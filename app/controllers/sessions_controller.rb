e2a39a263c429efd44af26c5ad6f394a35f455a6
mannykary <mrkaryam@gmail.com>
2014-05-21 22:14:26 -0700

    updated specs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    #user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by_email(params[:email].downcase)
+    if user && user.authenticate(params[:password])
+      session[:user] = user.id
       sign_in user
       redirect_to user
     else
